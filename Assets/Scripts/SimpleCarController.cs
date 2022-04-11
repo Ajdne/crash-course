e4f2eb0f5dd07910bf0f513d79f438bf641ad815
Srđan Radujko <62765673+Ajdne@users.noreply.github.com>
2022-04-11 21:38:36 +0200

    Movement tweaks

--- Assets/Scripts/SimpleCarController.cs
+++ Assets/Scripts/SimpleCarController.cs
@@ -20,28 +20,33 @@ public class SimpleCarController : MonoBehaviour {
     public float currentX;
     public float currentY;
     public float currentZ;
-    public float turnSpeed = 1;
+    public float turnSpeed = 2;
+    public float speed = 3;
     // *************************************
 
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque * 3; //Input.GetAxis("Vertical");
+        float motor = maxMotorTorque * 1; //Input.GetAxis("Vertical");
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
         
         // trying to avoid getting stuck into objects. Now the car can always rotate, no matter the wheels
         transform.Rotate(new Vector3(0, steering * turnSpeed * Time.deltaTime, 0));
 
-
         foreach (AxleInfo axleInfo in axleInfos) {
 
-            if (axleInfo.steering) {
+            if (axleInfo.steering) {                     // Steering part
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.rightWheel.steerAngle = steering;
             }
 
-            if (axleInfo.motor) {
+            if (axleInfo.motor) {                    // Acceleration
                 axleInfo.leftWheel.motorTorque = motor;
                 axleInfo.rightWheel.motorTorque = motor;
+            }
+
+            // THIS NEED FIXIN'
+            if ( motor > maxMotorTorque * 0.5) {    
+                transform.Translate(new Vector3(0, 0, speed * Time.deltaTime), Space.Self);
             }
         }
 
